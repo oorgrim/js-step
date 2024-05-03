8c1c0f4fd9007899de9bdb72884422f494b4354a
oorgrim <99543878+oorgrim@users.noreply.github.com>
2024-05-03 21:32:58 +0500

    May-3

---
+++ classwork/May-03/first.js
@@ -0,0 +1,5 @@
+// let lon = prompt('введите долготу')
+// let lat = prompt('введите широту')  
+
+
+const smth = new XMLHttpRequest();
---
+++ classwork/May-03/main.js
@@ -0,0 +1,31 @@
+// let obj = {
+//     name: 'Alex',
+//     age: 25,
+//     email: 'alex02@mail.ru'
+// }
+// console.log(obj);
+// // let obj2 = JSON.stringify(obj)
+// let obj2 = JSON.stringify(obj, null, 30)
+// console.log(obj2);
+// let obj3 = JSON.parse(obj2)
+// console.log(obj3);
+
+// let request = new XMLHttpRequest();
+// request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=Almaty&units=metric&APPID=b03a2cfad336d11bd9140ffd92074504');
+// request.send();
+
+// https://api.github.com/users/oorgrim
+
+// let request = new XMLHttpRequest();
+// request.open('GET', 'https://api.github.com/users/oorgrim');
+// request.send();
+
+
+    const smth = new XMLHttpRequest();
+
+    smth.open("GET", "https://api.github.com/users/oorgrim")
+
+    smth.onload = () => {
+        console.log(smth);
+    }
+    smth.send() 
